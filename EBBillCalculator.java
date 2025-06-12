ecc0850d1dd2b0ae913dc6b8b9a21051730525f1
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-06-12 22:09:21 +0530

    Update EBBillCalculator.java

--- EBBillCalculator.java
+++ EBBillCalculator.java
@@ -1,3 +1,14 @@
+// Vinay lives in an apartment where the electricity cost is calculated based on usage:
+
+// First 100 units: ₹1.5 per unit
+
+// Next 100 units (101–200): ₹2.5 per unit
+
+// Beyond 200 units: ₹3.5 per unit
+
+// Vinay used 275 units this month. His friend Suresh is curious: "How much will Vinay’s electricity bill be this month?"
+
+
 import java.util.*;
 
 public class EBBillCalculator
